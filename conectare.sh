997fa17bb0ad4a8676dc6f378bdbae0373408fa6
AdascaliteiGabriela <gabriela.adascalitei1@gmail.com>
2024-05-25 16:45:37 +0300

    Create conectare.sh

---
+++ conectare.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "Introdu username"
+read username
+user_info=$(grep "^$username," .users.csv)
+if [ -n "$user_info" ]; then
+    corect=$(echo $user_info | cut -d',' -f2)
+    echo "Introdu parola"
+    read password
+    if [ "$password" == "$corect" ]; then
+        echo "Conectare reusita."
+        cd "$username"
+    else
+        echo "Parola gresita."
+    fi
+else
+    echo "Utilizator gresit. Iti sugeram inregistrarea!"
+fi
