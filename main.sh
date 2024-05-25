79cdbbad0685f50cfb2cef490dce2a6cc9bdeada
AdascaliteiGabriela <gabriela.adascalitei1@gmail.com>
2024-05-25 20:13:16 +0300

    Update and rename login.sh to main.sh

--- main.sh
+++ main.sh
@@ -1,10 +1,9 @@
-
 #!/bin/bash
-echo "Bine ai venit! Pentru conectare apasa 1, pentru inregistrare apasa 0!"
+printf "Bine ai venit! Alege o optiune: \n 1. Conectare \n 2. Inregistrare \n 3. Iesire \n Ce ai dori sa faci? [1-3]:  "
 read ok
 if [ $ok -eq 1 ]; then
         source conectare.sh
-elif [ $ok -eq 0 ]; then
+elif [ $ok -eq 2 ]; then
         source  inregistrare.sh
 else
         echo "Am inteles, nu doresti sa te conectezi/inregistrezi. Sa ai o zi buna!"
