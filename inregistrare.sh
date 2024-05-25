44488d6f4bedefc89e384fc881713baf70805639
AdascaliteiGabriela <gabriela.adascalitei1@gmail.com>
2024-05-25 18:08:18 +0300

    Update inregistrare.sh

--- inregistrare.sh
+++ inregistrare.sh
@@ -10,12 +10,11 @@ if grep -q "^username," users.csv; then
         read mail
         verificare="^[a-zA-Z0-9._%+-]+@yahoo\.com$"
         if [[ $mail =~ $verificare ]]; then
-          echo "mail corect "
           echo "$username,$parola,$mail," >> users.csv
           mkdir "$username"
           echo "Utilizator creat!"
           cd "$username"
         else
-          echo "Adresa de mail incorecta"
+          echo "Adresa de mail incorecta. Utilizatorul nu a fost creat!"
         fi
     fi
