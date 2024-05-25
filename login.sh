cd7b9ba5b676e7f7e962ed3f334d44fd72dcb42b
AdascaliteiGabriela <gabriela.adascalitei1@gmail.com>
2024-05-25 16:49:50 +0300

    Update and rename inregistrare to login.sh

---
+++ login.sh
@@ -0,0 +1,14 @@
+
+#!/bin/bash
+echo "Bine ai venit! Pentru conectare apasa 1, pentru inregistrare apasa 0!"
+read ok
+if [ $ok -eq 1 ]; then
+        source .conectare.sh
+elif [ $ok -eq 0 ]; then
+        echo "Alege un username"
+        source  .inregistrare.sh
+else
+        echo "Am inteles, nu doresti sa te conectezi/inregistrezi. Sa ai o zi buna!"
+fi
+
+
