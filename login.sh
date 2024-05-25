c4a9b07791ab740415292a66c091ba248e088fae
AdascaliteiGabriela <gabriela.adascalitei1@gmail.com>
2024-05-25 17:17:16 +0300

    Update login.sh

--- login.sh
+++ login.sh
@@ -3,10 +3,10 @@
 echo "Bine ai venit! Pentru conectare apasa 1, pentru inregistrare apasa 0!"
 read ok
 if [ $ok -eq 1 ]; then
-        source .conectare.sh
+        source conectare.sh
 elif [ $ok -eq 0 ]; then
         echo "Alege un username"
-        source  .inregistrare.sh
+        source  inregistrare.sh
 else
         echo "Am inteles, nu doresti sa te conectezi/inregistrezi. Sa ai o zi buna!"
 fi
