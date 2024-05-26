14d695fe32a6b00c3eef5d10d82504f323712ae4
Bianca B <52595982+jinnxis@users.noreply.github.com>
2024-05-26 23:46:19 +0300

    Create logged_menu.sh

---
+++ logged_menu.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+username="$1"
+echo "Welcome, $username!"
+echo "Type 'logout' to log out."
+
+while true; do
+
+    read -p "$(pwd): " command
+
+    if [ "$command" == "logout" ]; then
+        echo "Logging out..."
+        awk -v user="$username" '$1 != user' ../../logged_in_users.csv > temp_file && mv temp_file ../../logged_in_users.csv
+    cd ..
+        break
+    else
+        $command
+    fi
+done
