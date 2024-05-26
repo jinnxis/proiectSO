7801db3eb648c0b9b3cf91641dc58f8a039014d9
Bianca B <52595982+jinnxis@users.noreply.github.com>
2024-05-27 00:37:09 +0300

    Update inregistrare.sh

--- inregistrare.sh
+++ inregistrare.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
+
 echo "Introdu username-ul dorit:"
 read username
+
 if [ ! -d "Utilizatori" ]; then
     mkdir -p "Utilizatori"
 fi
+
 if grep -q "^$username," users.csv; then
-        echo "Username deja folosit."
+    echo "Username deja folosit."
+else
+    echo "Introdu parola:"
+    read -s parola
+    echo "Introdu adresa de mail:"
+    read mail
+    verificare="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$"
+    if [[ $mail =~ $verificare ]]; then
+        highest_id=$(awk -F',' 'NR>1 && $4+0==$4 {print $4}' users.csv | sort -rn | head -n 1)
+        id=$((highest_id + 1))
+        echo "$username,$parola,$mail,$id" >> users.csv
+        mkdir "Utilizatori/$username"
+        echo "Utilizator creat!"
     else
-        echo "Introdu parola:"
-        read -s parola
-        echo "Introdu adresa de mail:"
-        read mail
-        verificare="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$"
-        if [[ $mail =~ $verificare ]]; then
-          highest_id=$(awk -F',' 'NR>1 && $4+0==$4 {print $4}' users.csv | sort -rn | head -n 1)
-          id=$((highest_id + 1))
-          echo "$username,$parola,$mail,$id" >> users.csv
-          mkdir "Utilizatori/$username"
-          echo "Utilizator creat!"
-        else
-          echo "Adresa de mail incorecta. Utilizatorul nu a fost creat!"
-        fi
+        echo "Adresa de mail incorecta. Utilizatorul nu a fost creat!"
     fi
+fi
