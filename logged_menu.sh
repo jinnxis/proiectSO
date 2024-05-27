f79aa86ad6b506dfb792cef640f825bf939aebbf
Bianca B <52595982+jinnxis@users.noreply.github.com>
2024-05-27 21:23:11 +0300

    Update logged_menu.sh

--- logged_menu.sh
+++ logged_menu.sh
@@ -2,8 +2,9 @@
 
 username="$1"
 echo "Bine ai venit, $username!"
-echo "Scrie 'logout' ca sa te deconectezi."
-echo "Scrie 'source ../../mesaje.sh' pentru a transmite un mesaj altui utilizator."
+echo "Scrie 'logout' pentru a te deconecta."
+echo "Scrie 'raport' pentru a genera raportul."
+echo "Scrie 'mesaj' pentru a transmite un mesaj altui utilizator."
 
 while true; do
 
@@ -15,7 +16,22 @@ while true; do
     cd ..
     cd ..
         break
-    else
+    elif [ "$command" == "raport" ]; then
+        touch raport.txt
+        truncate -s 0 raport.txt
+
+        nr_fis=$(find . -type f | wc -l)
+        nr_dir=$(find . -type d | wc -l)
+        dim_disc=$(du -sh . | cut -f1)
+
+        echo "Raportul utilizatorului: $username" >> raport.txt
+        echo "Numarul de fisiere: $nr_fis" >> raport.txt
+        echo "Numarul de directoare: $nr_dir" >> raport.txt
+        echo "Dimensiunea pe disc: $dim_disc" >> raport.txt
+        echo "Raportul a fost generat in directorul $(pwd)."
+     elif [ "$command" == "mesaj" ]; then
+        source ../../mesaje.sh
+     else
         $command
     fi
 done
