0aa286771162dfc5aa1f01f90a2700a0ab476105
AdascaliteiGabriela <gabriela.adascalitei1@gmail.com>
2024-05-27 20:31:12 +0300

    Update logged_menu.sh

--- logged_menu.sh
+++ logged_menu.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 username="$1"
-echo "Welcome, $username!"
-echo "Type 'logout' to log out."
+echo "Bine ai venit, $username!"
+echo "Scrie 'logout' ca sa te deconectezi."
+echo "Scrie 'source ../../mesaje.sh' pentru a transmite un mesaj altui utilizator."
 
 while true; do
 
     read -p "$(pwd): " command
 
     if [ "$command" == "logout" ]; then
-        echo "Logging out..."
+        echo "Deconectare..."
         awk -v user="$username" '$1 != user' ../../logged_in_users.csv > temp_file && mv temp_file ../../logged_in_users.csv
     cd ..
     cd ..
